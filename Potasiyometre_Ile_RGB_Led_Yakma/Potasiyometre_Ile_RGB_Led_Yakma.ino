e965520181b0970321859262aeccbff4789e8198
Yunus Emre KARADAŞ <45971663+EmiRogLu25@users.noreply.github.com>
2020-02-08 22:51:32 +0300

    Update Potasiyometre_Ile_RGB_Led_Yakma.ino

--- Potasiyometre_Ile_RGB_Led_Yakma/Potasiyometre_Ile_RGB_Led_Yakma.ino
+++ Potasiyometre_Ile_RGB_Led_Yakma/Potasiyometre_Ile_RGB_Led_Yakma.ino
@@ -1,12 +1,13 @@
-#define blue 9
-#define green 10
-#define red 11
-#define potBlue A0
-#define potGreen A1
-#define potRed A2
+#define blue 9//RGB ledin mavi bacagini 9.pine bagliyoruz
+#define green 10//RGB ledin yesil bacagini 10.pine bagliyoruz
+#define red 11//RGB ledin kirmizi bacagini 11.pine bagliyoruz
+#define potBlue A0//Mavi rengin degeri icin A0 pinine potansiyometre bagliyoruz
+#define potGreen A1//Yesil rengin degeri icin A1 pinine potansiyometre bagliyoruz
+#define potRed A2//Kirmizi rengin degeri icin A2 pinine potansiyometre bagliyoruz
 
 void setup() 
 {  
+  //RGB led icin renkleri cikis veriyoruz 
   pinMode(blue , OUTPUT);
   pinMode(green , OUTPUT);
   pinMode(red , OUTPUT);
@@ -14,14 +15,18 @@ void setup()
 
 void loop() 
 {  
+  //potlardan gelen verileri degiskenlere aktariyoruz
   int blueValue = analogRead(potBlue);
   int greenValue = analogRead(potGreen);
   int redValue = analogRead(potRed);
 
+  //potlardan gelen 0-1023 araligini 0-255 araligina ceviriyoruz
+  //cunku RGB led 0-255 araliginda calisir
   blueValue = map(blueValue , 0 , 1023 , 0 , 255);
   greenValue = map(greenValue , 0 , 1023 , 0 , 255);
   redValue = map(redValue , 0 , 1023 , 0 , 255);
   
+  //potlardan okunan degerleri RGB lede yaziyoruz
   analogWrite(blue , blueValue);
   analogWrite(green , greenValue);
   analogWrite(red , redValue);
