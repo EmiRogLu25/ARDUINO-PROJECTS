6beed074b1369a64f2baeb537d2c5af08927ed1c
Yunus Emre KARADAŞ <45971663+EmiRogLu25@users.noreply.github.com>
2020-02-08 23:14:45 +0300

    Update Ultrsonic_Sensor_ile_Park_Sensoru_.ino

--- Ultrsonic_Sensor_ile_Park_Sensoru_/Ultrsonic_Sensor_ile_Park_Sensoru_.ino
+++ Ultrsonic_Sensor_ile_Park_Sensoru_/Ultrsonic_Sensor_ile_Park_Sensoru_.ino
@@ -1,19 +1,22 @@
-#define echoPin 6
-#define trigPin 7
-#define buzzerPin 8
-#define ledOne 10
-#define ledTwo 11
-#define ledThree 12
+#define echoPin 6//HC06 ultrasonic sensorun echo pini giris pinidir yani dalgayi alan pindir
+#define trigPin 7//HC06 ultrasonic sensorun trig pini cikis pinidir yadi dalgayi ureten pindir
+#define buzzerPin 8//Buzzeri 8. pine bagladik
+//Mesafeninin siddetini gostermek icin uc led kullanildi
+#define ledOne 10//Birinci led 10.pine bagladik
+#define ledTwo 11//Ikinci ledi 11.pine bagladik
+#define ledThree 12//Ucuncu ledi 12.pine bagladik
 
+//HC06 ultrasonic sensorun hangi aralikta olcum yapacagini belirledik
 int maximumRange = 25;
 int minimumRange = 0;
 
 void setup() 
 {
-  pinMode(echoPin , INPUT);
-  pinMode(trigPin , OUTPUT);
-  pinMode(buzzerPin , OUTPUT);
+  pinMode(echoPin , INPUT);//HC06 echo pini dalga alan pin oldugundan giris pinidir
+  pinMode(trigPin , OUTPUT);//HC06 trig pini dalga ureten pin oldugundan cikis pinidir
+  pinMode(buzzerPin , OUTPUT);//Buzzer ses ureten pin oldugundan cikis pinidir
 
+  //ledler cikis pinidir
   pinMode(ledOne , OUTPUT);
   pinMode(ledTwo , OUTPUT);
   pinMode(ledThree , OUTPUT);
@@ -21,26 +24,31 @@ void setup()
 
 void loop() 
 {
+  //Mesafeyi olcer ve mesafeye gore ledi ve lambayı yakar
   int olcum = mesafe(minimumRange , maximumRange);
   melodi(olcum*10);
   lambaYak(olcum);
 }
 
+//Bu metod HC06 ultrasonic sensoru sayesinde mesafeyi olcer ve olculen deger max ve min deger araligindaysa dondurulur
 int mesafe(int minRange , int maxRange)
 {
   long duration , distance;
 
+  //HC06 tarafindan dalga uretilir ve uretilen dalga cisme carptiginda geri doner ve dalga HC06 tarafindan geri alinir 
   digitalWrite(trigPin , LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin , HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin , LOW);
 
+  //mesafe olculur
   duration = pulseIn(echoPin ,HIGH);
   distance = duration / 58.2;
 
   delay(50);
 
+  //Olculen deger max-min deger araligindaysa geri donus yapilir
   if(distance >= maxRange || distance <= minRange)
   {
     return 0;
@@ -51,6 +59,7 @@ int mesafe(int minRange , int maxRange)
   }
 }
 
+//Bu metod buzzerdan mesafeye gore melodi calar
 void melodi(int dly)
 {
   tone(buzzerPin , 440);
@@ -59,8 +68,10 @@ void melodi(int dly)
   delay(dly);
 }
 
+//Bu metod mesafeye gore ledleri yakar
 void lambaYak(int dstnc)
 { 
+  //Eğer mesafe 18-25 cm araligindaysa bir tane led yanar
   if(dstnc > 18 && dstnc <= 25)
   {
     digitalWrite(ledOne , HIGH);
@@ -70,6 +81,7 @@ void lambaYak(int dstnc)
     digitalWrite(ledOne , LOW);
   }
   
+  ////Eğer mesafe 5-18 cm araligindaysa iki tane led yanar
   if(dstnc > 5 && dstnc <= 18)
   {
     digitalWrite(ledOne , HIGH);
@@ -80,6 +92,7 @@ void lambaYak(int dstnc)
     digitalWrite(ledTwo , LOW);
   }
   
+  //Eğer mesafe 0-5 cm araligindaysa uc tane led yanar
   if(dstnc > 0 && dstnc <= 5)
   {
     digitalWrite(ledOne , HIGH);
